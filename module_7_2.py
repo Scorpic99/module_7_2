9bd0c560e524106dd05ac47cdc66fdc0b0edf44b
Александр <116272931+Scorpic99@users.noreply.github.com>
2024-10-26 20:16:12 +0300

    Update module_7_2_up.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,15 +1,11 @@
 def custom_write(file_name, strings):
-    my_file = open(file_name, 'w', encoding='utf-8')
+    my_file = open(file_name, 'w+', encoding='utf-8')
     my_dict = {}
     for i in range(len(strings)):
-        my_file.write(strings[i])
-        for_convert = my_file.tell()
-        start_str_byte = int.to_bytes(for_convert)
-        start_str_byte = start_str_byte.decode()
+        cursor_pos = my_file.tell()
+        my_file.write(strings[i] + '\n')
         my_file.seek(0, 2)
-        my_file.write('\n')
-        #my_file.seek(0, 2)
-        my_dict.update(dict({(i + 1, start_str_byte):strings[i]}))
+        my_dict.update(dict({(i + 1, cursor_pos):strings[i]}))
     my_file.close()
     return my_dict
 
